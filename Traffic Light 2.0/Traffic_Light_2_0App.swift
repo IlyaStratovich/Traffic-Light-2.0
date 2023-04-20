1e954de272f8bd222cfdb2e38fc48e0a66c5c32b
Ilya Stratovich <ilyastratovitch@yandex.by>
2023-04-20 21:43:03 +0300

    Initial Commit

---
+++ Traffic Light 2.0/Traffic_Light_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_Light_2_0App.swift
+//  Traffic Light 2.0
+//
+//  Created by Илья Стратович on 20.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_Light_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
